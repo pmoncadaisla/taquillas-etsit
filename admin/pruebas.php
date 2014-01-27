0060996da843a7bf6d76faba59aeedddb3bff642
pmoncadaisla <pmoncadaisla@gmail.com>
2014-01-28 00:28:20 +0100

    Public release

--- admin/pruebas.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require("../taquillas.functions.php");
-require_once("../bbdd.php");
-
-echo imprimir("http://impresora:Impresora*1029@canival.dat.etsit.upm.es/~pmoncada/taquillas/admin/index.php?pag=infocancelacion&cancelacion=1&print");
-
-
-?>
